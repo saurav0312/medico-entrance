3923df929e721b13a5142c8f75be18694c273565
Saurav Kumar Chaudhary <sauravchaudhary0312@gmail.com>
2022-01-28 23:31:20 +0530

    Test Qustions and StartTest changes

--- src/app/starttest/starttest.component.ts
+++ src/app/starttest/starttest.component.ts
@@ -12,6 +12,7 @@ export class StarttestComponent implements OnInit {
 
   @Input() testId!: string;
   testData!: MockTest;
+  counter = 0;
 
   constructor(
     private router : Router,
@@ -29,4 +30,21 @@ export class StarttestComponent implements OnInit {
     })
   }
 
+  increaseCounter(): void{
+    this.counter++;
+    if(this.counter === this.testData.questions.length ){
+      this.counter = this.testData.questions.length-1;
+    }
+  }
+
+  decreaseCounter(): void{
+    this.counter --;
+    if(this.counter === -1){
+      this.counter = 0;
+    }
+  }
+
+  changeQuestion(questionIndex: number): void{
+    this.counter = questionIndex
+  }
 }
