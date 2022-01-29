528aed12371a8c9f05b76ac10377f2515a184085
Saurav Kumar Chaudhary <sauravchaudhary0312@gmail.com>
2022-01-29 17:02:25 +0530

    Score calculation working

--- src/app/starttest/starttest.component.ts
+++ src/app/starttest/starttest.component.ts
@@ -1,18 +1,26 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { MockTest } from '../interface/mockTest';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../service/auth.service';
+import { interval, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-starttest',
   templateUrl: './starttest.component.html',
   styleUrls: ['./starttest.component.css']
 })
-export class StarttestComponent implements OnInit {
+export class StarttestComponent implements OnInit, OnDestroy {
 
   @Input() testId!: string;
   testData!: MockTest;
   counter = 0;
+  totalTimeRemaining = 60;
+  countDownSubscription$!: Subscription;
+  testFinished : boolean = false;
+  noOfQuestionsAnsweredCorrectly! : number;
+  noOfQuestionsAnsweredIncorrectly! : number;
+  noOfQuestionsAttempted! : number;
+  noOfQuestionsUnattempted! : number
 
   constructor(
     private router : Router,
@@ -25,9 +33,43 @@ export class StarttestComponent implements OnInit {
       this.testId = <string>params.data
       this.authService.getMockTestByID(this.testId).subscribe(response=>{
         this.testData= response;
+        // this.countDownSubscription$ = interval(1000).subscribe(x =>{
+        //   this.totalTimeRemaining--;
+        //   if(this.totalTimeRemaining === 0){
+        //     this.testFinished= true;
+        //     this.evaluateMarks(this.testData);
+        //     console.log(this.testData)
+        //   }
+        // })
       })
       console.log("Query data", this.testId)
     })
+  }
+
+  evaluateMarks(testResult: MockTest): void{
+    let questionsAnsweredCorrectly = 0;
+    let questionsAnsweredIncorrectly = 0;
+    let questionsAttempted = 0;
+
+    testResult.questions.forEach(question =>{
+      if(question.selectedOption!== undefined){
+        questionsAttempted++;
+        if(question.selectedOption === question.correctAnswer){
+          questionsAnsweredCorrectly++;
+        }
+        else{
+          questionsAnsweredIncorrectly++;
+        }
+      }
+    });
+    this.noOfQuestionsAnsweredCorrectly = questionsAnsweredCorrectly;
+    this.noOfQuestionsAnsweredIncorrectly = questionsAnsweredIncorrectly;
+    this.noOfQuestionsAttempted = questionsAttempted;
+    this.noOfQuestionsUnattempted = testResult.questions.length - questionsAttempted;
+  }
+
+  ngOnDestroy(){
+    this.countDownSubscription$.unsubscribe();
   }
 
   increaseCounter(): void{
