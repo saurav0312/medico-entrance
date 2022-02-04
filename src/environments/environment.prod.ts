01a573bcc0402fe0bac767350f77300311cc6148
Saurav Kumar Chaudhary <sauravchaudhary0312@gmail.com>
2022-02-04 14:31:17 +0530

    navigation bar changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDgGok4mBvSF2tusIQJ0G3tov5ocnEwzl4",
+    authDomain: "entranceexam-d5150.firebaseapp.com",
+    projectId: "entranceexam-d5150",
+    storageBucket: "entranceexam-d5150.appspot.com",
+    messagingSenderId: "96634587593",
+    appId: "1:96634587593:web:1f168f48b8f4ab0180c018",
+    measurementId: "G-XFBEDSDFWN",
+    realtimeDatabaseUrl: "https://entranceexam-d5150-default-rtdb.firebaseio.com/"
+  }
 };
